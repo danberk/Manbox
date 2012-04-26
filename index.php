1b5c582b26f9f573423f735abb766c8b0a0c34f2
Dan Berk <dan@lastpictures.com>
2012-04-25 20:19:20 -0400

    Filled out index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+  // Start the session
+  require_once('startsession.php');
+
+  // Insert the page header
+  $page_title = 'Stay clean, stay classy';
+  require_once('header.php');
+
+  require_once('appvars.php');
+  require_once('connectvars.php');
+
+  // Show the navigation menu
+  require_once('navigation.php');
+?>
+
+<div id="container">
+	<?php
+	require_once('usersubmissionform.php');
+	require_once('indexcontent.php'); 
+	?>
+</div>
+<?php
+	require_once('footer.php');
+?>
